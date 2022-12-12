539a94815efc76874a1fe884f60e3345ff45c85d
RAJESH SANNALA <rajeshkumar.s@bristlecone.com>
2022-12-12 16:49:35 +0530

    fixed issues with header and footer components logos with stylings

--- src/components/HeaderComponent.tsx
+++ src/components/HeaderComponent.tsx
@@ -26,6 +26,7 @@ type HeaderComponentProps = ComponentProps & {
     formHeader: Field<string>;
     formBody: Field<string>;
     checkboxItems: updateMeFormItems[];
+    logo: Field<string>;
   };
 };
 
@@ -63,10 +64,14 @@ const HeaderComponent = (props: HeaderComponentProps): JSX.Element => {
           <Navbar.Toggle
             aria-controls="offcanvasNavbar-expand-xl"
             className="navbarToggleIcon"
-          ></Navbar.Toggle>
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="white" className="bi bi-list" viewBox="0 0 16 16">
+              <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+            </svg>
+          </Navbar.Toggle>
           <Navbar.Brand href="#">
             <img
-              src="../data/media/img/logoMahindra.png"
+              src={props.fields.logo.value}
               width="130"
               height="20"
               alt="Mahindra"
